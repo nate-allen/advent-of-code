93dfaca9a6ba78f394f36122d2a1f862fede87fe
Nate Allen <nate.allen@automattic.com>
2023-12-15 18:12:44 -0500

    Add solutions for part 1 and 2

--- 2023/day-15.php
+++ 2023/day-15.php
@@ -1,16 +1,22 @@
 <?php
 
 /**
- * Day 15:
+ * Day 15: Lens Library
  */
 class Day15 {
-
 	/**
 	 * The puzzle data.
 	 *
 	 * @var array
 	 */
 	private array $data;
+
+	/**
+	 * Array representing the 256 boxes
+	 *
+	 * @var array
+	 */
+	private array $boxes;
 
 	/**
 	 * Constructor to parse the data.
@@ -23,21 +29,122 @@ class Day15 {
 	}
 
 	/**
-	 * Part 1:
+	 * Part 1: Find the sum of the HASH algorithm results for each step.
 	 *
 	 * @return int The answer.
 	 */
 	public function part_1(): int {
-		return 0;
+		$sum = 0;
+
+		foreach ( $this->data as $step ) {
+			$sum += $this->compute_hash( $step );
+		}
+
+		return $sum;
 	}
 
 	/**
-	 * Part 2:
+	 * Part 2: Computes the total focusing power of the lens configuration.
 	 *
-	 * @return int The answer.
+	 * @return int The total focusing power.
 	 */
 	public function part_2(): int {
-		return 0;
+		$this->boxes = array_fill( 0, 256, [] );
+
+		foreach ( $this->data as $step ) {
+			$this->process_step( $step );
+		}
+
+		return $this->calculate_focusing_power();
+	}
+
+	/**
+	 * Computes the HASH value for a given string
+	 *
+	 * @param string $string The string to compute the HASH for.
+	 *
+	 * @return int The HASH value.
+	 */
+	private function compute_hash( string $string ): int {
+		$current_value = 0;
+
+		foreach ( str_split( $string ) as $char ) {
+			$ascii         = ord( $char );
+			$current_value = ( $current_value + $ascii ) * 17 % 256;
+		}
+
+		return $current_value;
+	}
+
+	/**
+	 * Processes each step in the initialization sequence.
+	 *
+	 * @param string $step The step to process.
+	 */
+	private function process_step(string $step): void {
+		$label        = substr( $step, 0, str_contains( $step, '=' ) ? strpos( $step, '=' ) : strpos( $step, '-' ) );
+		$index        = $this->compute_hash( $label );
+		$operation    = $step[ strlen( $label ) ];
+		$focal_length = '=' === $operation ? intval( substr( $step, strlen( $label ) + 1 ) ) : null;
+
+		if ( $operation === '-' ) {
+			$this->remove_lens( $index, $label );
+		} else {
+			$this->insert_lens( $index, $label, $focal_length );
+		}
+	}
+
+	/**
+	 * Inserts a lens into the specified box.
+	 *
+	 * @param int    $index        The index of the box.
+	 * @param string $label        The label of the lens.
+	 * @param int    $focal_length The focal length of the lens.
+	 */
+	private function insert_lens( int $index, string $label, int $focal_length ): void {
+		$found = false;
+
+		foreach ( $this->boxes[ $index ] as &$lens ) {
+			if ( $lens['label'] === $label ) {
+				$lens['focal_length'] = $focal_length;
+				$found                = true;
+				break;
+			}
+		}
+
+		if ( ! $found ) {
+			$this->boxes[ $index ][] = [ 'label' => $label, 'focal_length' => $focal_length ];
+		}
+	}
+
+	/**
+	 * Removes a lens from the specified box.
+	 *
+	 * @param int    $index The index of the box.
+	 * @param string $label The label of the lens to remove.
+	 */
+	private function remove_lens( int $index, string $label ): void {
+		$this->boxes[ $index ] = array_values( array_filter(
+			$this->boxes[ $index ],
+			fn( $lens ) => $lens['label'] !== $label
+		) );
+	}
+
+	/**
+	 * Calculates the total focusing power of the lens configuration.
+	 *
+	 * @return int The total focusing power.
+	 */
+	private function calculate_focusing_power(): int {
+		$power = 0;
+
+		foreach ( $this->boxes as $index => $lenses ) {
+			foreach ( $lenses as $slotIndex => $lens ) {
+				$power += ( $index + 1 ) * ( $slotIndex + 1 ) * $lens['focal_length'];
+			}
+		}
+
+		return $power;
 	}
 
 	/**
@@ -48,11 +155,9 @@ class Day15 {
 	 */
 	private function parse_data(string $test, int $part): void {
 		$file       = $test ? '/data/day-15-test.txt' : '/data/day-15.txt';
-		$lines      = explode("\n", trim(file_get_contents(__DIR__ . $file)));
-		$this->data = array_map(fn($line) => str_split($line), $lines);
+		$this->data = explode( ',', trim( file_get_contents( __DIR__ . $file ) ) );
 	}
 }
-
 
 // Prompt which part to run and if it should use the test data.
 while ( true ) {
@@ -77,7 +182,7 @@ function part_1( $test = false ) {
 	$day15    = new Day15( $test, 1 );
 	$result   = $day15->part_1();
 	$end      = microtime( true );
-	$expected = $test ? 0 : 0;
+	$expected = $test ? 1320 : 517315;
 
 	printf( 'Total:    %s' . PHP_EOL, $result );
 	printf( 'Expected: %s' . PHP_EOL, $expected );
@@ -89,7 +194,7 @@ function part_2( $test = false ) {
 	$day15    = new Day15( $test, 2 );
 	$result   = $day15->part_2();
 	$end      = microtime( true );
-	$expected = $test ? 0 : 0;
+	$expected = $test ? 145 : 247763;
 
 	printf( 'Total:    %s' . PHP_EOL, $result );
 	printf( 'Expected: %s' . PHP_EOL, $expected );
