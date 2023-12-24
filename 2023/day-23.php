76806c6ee1d255b17ae6bc31e6dbecf83596ca12
Nate Allen <nate.allen@automattic.com>
2023-12-23 23:53:38 -0500

    Add day 23 template

---
+++ 2023/day-23.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * Day 23: A Long Walk
+ */
+class Day23 {
+	/**
+	 * The puzzle data.
+	 *
+	 * @var array
+	 */
+	private array $data;
+
+	/**
+	 * The puzzle part, 1 or 2.
+	 *
+	 * @var int
+	 */
+	private int $part;
+
+	/**
+	 * Whether or not to use the test data.
+	 *
+	 * @var bool
+	 */
+	private bool $is_test;
+
+	public function __construct(string $test, int $part) {
+		$this->parse_data($test);
+		$this->part = $part;
+		$this->is_test = $test;
+	}
+
+	public function part_1(): int {
+
+	}
+
+	public function part_2(): int {
+
+	}
+
+	private function parse_data(bool $test): void {
+		$file = $test ? '/data/day-23-test.txt' : '/data/day-23.txt';
+		$this->data = explode("\n", trim(file_get_contents(__DIR__ . $file)));
+
+	}
+}
+
+// Prompt which part to run and if it should use the test data.
+while ( true ) {
+	$part = trim( readline( 'Which part do you want to run? (1/2)' ) );
+	if ( function_exists( "part_$part" ) ) {
+		while ( true ) {
+			$test = trim( strtolower( readline( 'Do you want to run the test? (y/n)' ) ) );
+			if ( in_array( $test, array( 'y', 'n' ), true ) ) {
+				$test = 'y' === $test;
+				call_user_func( "part_$part", $test );
+				break;
+			}
+			echo 'Please enter y or n' . PHP_EOL;
+		}
+		break;
+	}
+	echo 'Please enter 1 or 2' . PHP_EOL;
+}
+
+function part_1( $test = false ) {
+	$start    = microtime( true );
+	$day23    = new Day23( $test, 1 );
+	$result   = $day23->part_1();
+	$end      = microtime( true );
+	$expected = $test ? 0 : 3716;
+
+	printf( 'Total:    %s' . PHP_EOL, $result );
+	printf( 'Expected: %s' . PHP_EOL, $expected );
+	printf( 'Time:     %s seconds' . PHP_EOL, round( $end - $start, 4 ) );
+}
+
+function part_2( $test = false ) {
+	$start    = microtime( true );
+	$day23    = new Day23( $test, 2 );
+	$result   = $day23->part_2();
+	$end      = microtime( true );
+	$expected = $test ? 16733044 : 616583483179597;
+
+	printf( 'Total:    %s' . PHP_EOL, $result );
+	printf( 'Expected: %s' . PHP_EOL, $expected );
+	printf( 'Time:     %s seconds' . PHP_EOL, round( $end - $start, 4 ) );
+}
